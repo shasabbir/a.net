5777aa2fffb0675950307e4087758328a1566cf5
shasabbir <shasabbir234@gmail.com>
2022-01-31 14:02:32 +0600

    person-product class work

---
+++ WebApplication2/WebApplication2/Controllers/PersonController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult home()
+        {
+            /*ViewBag.name = "Sabbir";
+            ViewBag.id = 1234;*/
+            Person p = new Person();
+            p.Name = "Sabbir-M";
+            p.Id = 1234;
+            return View(p);
+        }
+        public ActionResult list()
+        {
+            /*string[] names = { "nafis", "salman", "samin", "jamil" };
+            ViewBag.list=names;*/
+            Person[] p= new Person[10];
+            for(int i = 0; i < p.Length; i++)
+            {
+                p[i] = new Person();
+                p[i].Id = i+1;
+                p[i].Name = "person"+(i+1);
+            }
+            return View(p);
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult List()
+        {
+            List <Product> list = new List<Product> ();
+            for(int i = 0; i < 10; i++)
+            {
+                Product p = new Product ();
+                p.Id = i+1;
+                p.Name = "Product"+i.ToString();
+                list.Add (p);
+            }
+            return View(list);
+        }
+    }
+}
