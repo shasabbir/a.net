51efb0b3a30cffa24e9bc14e7c0e405a505cefd6
shasabbir <shasabbir234@gmail.com>
2022-02-14 22:11:35 +0600

    Extended Product Task

---
+++ productTask/productTask/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using productTask.Models.Database;
+using System.Web.Script.Serialization;
+
+namespace productTask.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            productEntities db = new productEntities();
+            var pl = db.products.ToList();
+            return View(pl);
+        }
+        public ActionResult Checkout()
+        {
+            var pr = Session["cart"].ToString();
+            List<product> plist = new JavaScriptSerializer().Deserialize<List<product>>(pr);
+            return View(plist);
+        }
+        public ActionResult Removefromcart(int id)
+        {
+            productEntities db = new productEntities();
+            product p = (from pp in db.products
+                         where pp.id.Equals(id)
+                         select pp).FirstOrDefault();
+            var pr = Session["cart"].ToString();
+            List<product> plist = new JavaScriptSerializer().Deserialize<List<product>>(pr);
+            plist.Remove(p);
+            string json = new JavaScriptSerializer().Serialize(plist);
+            Session["cart"] = json;
+            return RedirectToAction("Checkout");
+            //return View(plist);
+        }
+        public ActionResult Addtocart(int id)
+        {
+            productEntities db = new productEntities();
+            product p = (from pp in db.products
+                         where pp.id.Equals(id)
+                         select pp).FirstOrDefault();
+
+
+            if (Session["cart"] != null)
+            {
+                var pr = Session["cart"].ToString();
+                List<product> plist = new JavaScriptSerializer().Deserialize<List<product>>(pr);
+                plist.Add(p);
+                string json = new JavaScriptSerializer().Serialize(plist);
+                Session["cart"] = json;
+                return RedirectToAction("index");
+            }
+            else
+            {
+
+                List<product> plist = new List<product>();
+                plist.Add(p);
+
+                string json = new JavaScriptSerializer().Serialize(plist);
+                Session["cart"] = json;
+                return RedirectToAction("index");
+            }
+        }
+    }
+}
---
+++ productTask/productTask/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="productTask.MvcApplication" Language="C#" %>
